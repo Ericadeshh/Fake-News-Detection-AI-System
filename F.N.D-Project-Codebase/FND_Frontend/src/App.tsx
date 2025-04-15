4c99b49ddbce85e4c23f33f51abef6fb8230335f
Ericadeshh <129593214+Ericadeshh@users.noreply.github.com>
2025-04-16 00:51:47 +0300

    added Heade.tsx component

--- F.N.D-Project-Codebase/FND_Frontend/src/App.tsx
+++ F.N.D-Project-Codebase/FND_Frontend/src/App.tsx
@@ -1,10 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
 import NewsChecker from "./components/NewsChecker/NewsChecker";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import History from "./pages/History";
 
 function App() {
   return (
-    <div className="App">
-      <NewsChecker />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<NewsChecker />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/verify" element={<NewsChecker />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
